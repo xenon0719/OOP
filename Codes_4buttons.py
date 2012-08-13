5f8d22d9aa5366136132c1aa6ee2eefc6dba5a10
gabriel <gabriel_sapitula@yahoo.com>
2012-08-13 10:17:58 +0800

    4buttons in widget codes

---
+++ Codes_4buttons.py
@@ -0,0 +1,49 @@
+import tkinter
+
+class Application(tkinter.Frame):
+
+    def __init__(self,master):
+        tkinter.Frame.__init__(self,master)
+        self.pack()
+        self.CreateWidgets()
+        self.save()
+        self.load()
+        
+    def CreateWidgets(self):
+        self.textbox1=tkinter.Text()
+        self.textbox1.pack()
+        self.button1=tkinter.Button()
+        self.button1["command"]=self.button1_click
+        self.button1["text"]="click"
+        self.button1.pack()
+        self.savebutton=tkinter.Button()
+        self.savebutton["text"]="save"
+        self.savebutton["command"]=self.save
+        self.savebutton.pack()
+        self.loadbutton=tkinter.Button()
+        self.loadbutton["text"]="load"
+        self.loadbutton["command"]=self.load
+        self.loadbutton.pack()
+        self.quitbutton=tkinter.Button()
+        self.quitbutton["text"]="quit"
+        self.quitbutton["command"]=self.quit
+        self.quitbutton.pack()
+    def save(self):
+        pass
+        
+    def load(self):
+        pass
+        
+    def button1_click(self):
+        text=self.textbox1.get(0.0,tkinter.END)
+        
+        print(text.upper())
+        print(text.lower())
+        print(text.title())
+
+        
+
+root=tkinter.Tk()
+app=Application(master=root)
+app.mainloop()
+root.destroy()
